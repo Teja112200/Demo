b6db772fe437a0b91c03ac807ccf957985a45317
Teja112200 <teja112200@gmail.com>
2022-12-27 00:46:38 +0530

    Added registration page and database

--- Demo.py
+++ Demo.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+import sqlite3
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
@@ -10,8 +11,42 @@ def Demo():
 database = {'teja': '504', 'akhil': '521', 'akash': '513', 'sravan': '505'}
 
 
-@app.route('/form_login', methods=['GET', 'POST'])
-def home():
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route("/savedetails", methods=["POST", "GET"])
+def saveDetails():
+    msg = "msg"
+    if request.method == "POST":
+        try:
+            new_user = request.form["username"]
+            new_pwd = request.form["password"]
+            email = request.form["email"]
+            phone = request.form["phone"]
+            with sqlite3.connect("project_db.db") as con:
+                cur = con.cursor()
+
+                # con.execute('CREATE TABLE students (username TEXT, password TEXT, Email TEXT, phone TEXT)')
+                # print("Table created successfully")
+                # con.close()
+
+                cur.execute("INSERT into student_data (username, password , email , phone) values (?,?,?,?)",
+                            (new_user, new_pwd, email, phone))
+                con.commit()
+                msg = "Student successfully Added"
+        except:
+            con.rollback()
+            msg = "We can not add the student to the list"
+        finally:
+            return render_template("pass.html", a=msg, n=new_user, c=new_pwd, d=email, q=phone)
+            con.close()
+
+
+@app.route('/valid_login', methods=['POST', 'GET'])
+def valid_login():
+
     username = request.form['username']
 
     pwd = request.form['password']
